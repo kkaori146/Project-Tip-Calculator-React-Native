0f097776e69bbde1081838a7527ab8bf293e69d6
Kaminishikawahara KK <kkaori146@gmail.com>
2021-08-25 14:07:51 +0900

    Installing styled-components and folder organization

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+const Page = styled.SafeAreaView`
+  flex: 1;
+  jutify-content: center;
+  align-items: center;
+`;
+
+export default () => {
+  return (
+    <Page>
+
+    </Page>
+  );
+}
